46143d50440441ceff86dfc1a2b43d8dc14d071f
Evald Monastyrski <evaldmon@gmail.com>
2019-12-07 21:52:24 +0100

    Add Web Driver

--- gpro-http/src/main/kotlin/lt/em/http/GPROConnector.kt
+++ gpro-http/src/main/kotlin/lt/em/http/GPROConnector.kt
@@ -1,5 +1,7 @@
 package lt.em.http
 
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions
 import org.slf4j.LoggerFactory
 
 private val LOGGER = LoggerFactory.getLogger(GPROConnector::class.java)
@@ -12,12 +14,26 @@ class GPROConnector {
         val webDriverPath = if (isApple()) APPLE_WEB_DRIVER_PATH else WINODWS_WEB_DRIVER_PATH
         System.setProperty(WEB_DRIVER_KEY, webDriverPath)
     }
-    val propertyValues = PropertyValues()
+    private val propertyValues = PropertyValues()
     init {
         LOGGER.info("My property values: Clean Login: {}, Flush Memory: {}, Headless: {}",
             propertyValues.isCleanLogin,
             propertyValues.isFlushMemory,
             propertyValues.isHeadless)
+    }
+    private val options = ChromeOptions()
+    init {
+        if (propertyValues.isHeadless) {
+            options.addArguments("--headless",
+                "--disable-gpu",
+                "--window-size=1920,1200",
+                "--ignore-certificate-errors"
+            )
+        }
+    }
+    private val webDriver = ChromeDriver(options)
+    init {
+        webDriver.close()
     }
 }
 
