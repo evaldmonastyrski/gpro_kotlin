8f3c4bb0449f0bed92c3db1b3f6f983241f08bd8
Evald Monastyrski <evaldmon@gmail.com>
2019-12-08 21:53:24 +0100

    Add dependency on external calculator

--- gpro-core/src/main/kotlin/lt/em/core/SetupBridge.kt
+++ gpro-core/src/main/kotlin/lt/em/core/SetupBridge.kt
@@ -1,19 +1,37 @@
 package lt.em.core
 
+import lt.em.core.converters.Converter
 import lt.em.datamodel.Car
 import lt.em.datamodel.Driver
 import lt.em.datamodel.PractiseConditions
 import lt.em.datamodel.Setup
+import lt.em.gpro.calculator.SetupCalculator
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 private val LOGGER: Logger = LoggerFactory.getLogger(SetupBridge::class.java)
+private const val USE_EXTERNAL_VALUATION = true
 
 class SetupBridge {
     fun calculateSetup(driver: Driver, car: Car, practiseConditions: PractiseConditions): Setup {
-        val setup = Setup()
-        LOGGER.info("Calculator package is unavailable, default setup will be used: {}", setup)
-        return setup
+        if (USE_EXTERNAL_VALUATION) {
+            val converter = Converter()
+            val cSetup = SetupCalculator().calculate(
+                converter.convertDriver(driver),
+                converter.convertPractiseConditions(practiseConditions),
+                converter.convertCar(car)
+            )
+            var setup = Setup()
+            if (cSetup != null) {
+                setup = converter.convertSetup(cSetup)
+            }
+            LOGGER.info("Calculator package is available, setup for {}: {}",practiseConditions.trackName, setup)
+            return setup.copy()
+        } else {
+            val setup = Setup()
+            LOGGER.info("Calculator package is unavailable, default setup will be used: {}", setup)
+            return setup
+        }
     }
 }
 
